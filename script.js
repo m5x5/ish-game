043c15461cb3bce74a95335c679e713e3b21048c
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-06-28 23:04:16 -0600

    canavs movements

--- script.js
+++ script.js
@@ -1,16 +1,47 @@
-window.onload = () => {
-  const canvas = document.getElementById("canvas");
-  const c = canvas.getContext("2d");
+window.onload = function(){
+  var canvas = document.getElementById("canvas");
+  var c = canvas.getContext("2d");
   canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight - 200;
-  const controller = document.getElementById("controller");
+  canvas.height = window.innerHeight;
+  canvas.addEventListener("click", function(event){
+      this.x = event.pageX;
+      this.y = event.pageY;
+      if(this.x < canvas.width / 2 && this.y > canvas.height / 2.5){
+        //move backwards(left)
+      }
+      if(this.x > canvas.width / 2 && this.y > canvas.height / 2.5){
+        //move foreward(right)
+      }
+      if(this.y < canvas.height / 2.5){
+        //jump
+      }
+  });
+
   //Device width / height specifications
   if (window.innerHeight < 900) {
-    controller.style.fontSize = "5vh";
-  } else {
-    controller.style.fontSize = "10vh";
-  }
 
+  } else {
+    
+  }
+  function MainView(){
+    this.render = function(){
+      //player1.onload = function(){} 
+      //var sprite1 = new Image();
+      //sprite1.src = "";
+      //sprite1.onload = function(){}
+      //var sprite2 = new Image();
+      //sprite2.src = "";
+    }
+  }
+  var mainview = new MainView();
+  function animate(){
+    mainview.render();
+    requestAnimationFrame("animate");
+  }
+  animate();
+
+}
+/*
   let model = {
     clickCount: 0
   }
@@ -26,29 +57,28 @@ window.onload = () => {
 
   const mainView = {
     init: () => {
-
+      player1.onload = function(){alert(8)} 
+      var sprite1 = new Image();
+      sprite1.src =
+        "";
+      sprite1.onload = function(){}
+      var sprite2 = new Image();
+      sprite2.src =
+        "";
     }
   }
   let playerView = {
     init: () => {
-      this.positonY =
-        this.playerElem = document.getElementById('player');
       this.scoreElem = document.getElementById('score');
-      this.playerElem.addEventListener('click', () => {
-        octopus.incrementCounter();
-        this.scoreElem.innerHTML = model.clickCount;
-      });
+
     },
     move: (direction) => {
-      this.playerElem.style[direction] += 10 + 'px';
     }
   }
 
   let obstracleView = {
     init: () => {
-      this.upElem = document.getElementById('controller-up');
-      this.upElem.addEventListener('click', () => playerView.move('top'));
-      document.addEventListener('keydown', () => playerView.move('top'));
+
     }
   }
-}
+  */
