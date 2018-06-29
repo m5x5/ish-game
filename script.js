968bb800ba8fba3d1ef604318fc76b6c5ae16007
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-06-28 21:52:26 -0600

    commit2

--- script.js
+++ script.js
@@ -4,35 +4,36 @@ window.onload = () => {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight - 200;
   const controller = document.getElementById("controller");
-  //Device width/height specifications
+  //Device width / height specifications
   if (window.innerHeight < 900) {
     controller.style.fontSize = "5vh";
   } else {
     controller.style.fontSize = "10vh";
   }
-}
-let model = {
+
+  let model = {
     clickCount: 0
   }
   let octopus = {
     init: () => {
       playerView.init();
-      obstacleView.init();
+      obstracleView.init();
     },
     incrementCounter: () => {
       model.clickCount++;
     },
   }
-  
-  onload = () => {
-    octopus.init();
+
+  const mainView = {
+    init: () => {
+
+    }
   }
-  
   let playerView = {
     init: () => {
-      this.positonY = 
+      this.positonY =
+        this.playerElem = document.getElementById('player');
       this.scoreElem = document.getElementById('score');
-      //Whats the octopus object all about?
       this.playerElem.addEventListener('click', () => {
         octopus.incrementCounter();
         this.scoreElem.innerHTML = model.clickCount;
@@ -42,11 +43,12 @@ let model = {
       this.playerElem.style[direction] += 10 + 'px';
     }
   }
-  
-  let obstacleView = {
+
+  let obstracleView = {
     init: () => {
       this.upElem = document.getElementById('controller-up');
       this.upElem.addEventListener('click', () => playerView.move('top'));
       document.addEventListener('keydown', () => playerView.move('top'));
     }
   }
+}
