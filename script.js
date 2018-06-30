96f8a51233cc838bd9c93b086c97775e36fc593c
Ole113 <36930869+Ole113@users.noreply.github.com>
2018-06-30 13:05:30 -0600

    Added "A" to move left, "D" to move right, and "Space" to jump

--- script.js
+++ script.js
@@ -12,23 +12,25 @@ let controller = {
       this.x = event.pageX;
       this.y = event.pageY;
       if (this.x < canvas.width / 2 && this.y > canvas.height / 2.5) {
-        //move backwards(left)
+
+        //left
       }
       if (this.x > canvas.width / 2 && this.y > canvas.height / 2.5) {
-        //move foreward(right)
+        //right
       }
       if (this.y < canvas.height / 2.5) {
+
         //jump
       }
+    
     });
 
     //Device width / height specifications
     if (window.innerHeight < 900) {
-
+      
     } else {
-
+      
     }
-
     mainView.init();
     controller.animate();
   },
@@ -56,7 +58,8 @@ const mainView = {
     this.bug.addEventListener('load', () => { });
     this.bug.src = "bug.png";
     document.addEventListener('keydown', (e) => {
-      switch (e.key) {
+
+      switch (e.code) {
         case 'ArrowUp':
           if (this.playerY > 0 && this.jumpCount < 2) {
             this.playerY -= 100;
@@ -80,6 +83,29 @@ const mainView = {
             this.playerX += 5;
           }
           break;
+        case "KeyA": //left
+          if (this.playerX > 0) {
+            this.playerX -= 5;
+          }
+          break;
+        case "KeyD"://right
+          if (this.playerX + 100 < innerWidth) {
+            this.playerX += 5;
+          }
+          break;
+        case "Space"://jump
+          if (this.playerY > 0 && this.jumpCount < 2) {
+            this.playerY -= 100;
+            this.jumpCount++;
+          } else if(this.playerY == innerHeight - 100){
+            this.jumpCount = 0;
+          }
+          break;
+        case "KeyS"://down
+          if (this.playerY + 100 < innerHeight) {
+            this.playerY += 10;
+          }
+          break;
       }
     });
   },
@@ -91,7 +117,6 @@ const mainView = {
     c.drawImage(this.player, this.playerX, this.playerY, 100, 100);
   }
 }
-
 onload = () => {
   controller.init();
 }
