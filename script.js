8bdb2099e5e3ab6b92de65115b8125399101523a
Michael55555 <michaelpeters842@gmail.com>
2018-07-01 08:03:34 +0200

    Make the bugs dissappear

--- script.js
+++ script.js
@@ -4,32 +4,30 @@ let model = {
 
 let controller = {
   init: () => {
-    let canvas = document.getElementById("canvas");
-    this.c = canvas.getContext("2d");
+    let canvas = document.getElementById('canvas');
+    this.c = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    canvas.addEventListener("click", function (event) {
+    canvas.addEventListener('click', function (event) {
       this.x = event.pageX;
       this.y = event.pageY;
       if (this.x < canvas.width / 2 && this.y > canvas.height / 2.5) {
-
-        //left
+        // left
       }
       if (this.x > canvas.width / 2 && this.y > canvas.height / 2.5) {
-        //right
+        // right
       }
       if (this.y < canvas.height / 2.5) {
-
-        //jump
+        // jump
       }
-    
+
     });
 
-    //Device width / height specifications
+    // Device width / height specifications
     if (window.innerHeight < 900) {
-      
+
     } else {
-      
+
     }
     mainView.init();
     controller.animate();
@@ -48,23 +46,22 @@ const mainView = {
   init: () => {
     this.jumpCount = 0;
     this.bugX = 0;
-    this.bugY = 0;
+    this.bugY = innerHeight / 5;
     this.playerX = 0;
-    this.playerY = innerHeight - 100;
+    this.playerY = innerHeight / 5;
+    this.floor = new Image();
+    this.floor.src = 'floor.png';
     this.player = new Image();
-    this.player.addEventListener('load', () => { });
-    this.player.src = "developer.png";
+    this.player.src = 'developer.png';
     this.bug = new Image();
-    this.bug.addEventListener('load', () => { });
-    this.bug.src = "bug.png";
+    this.bug.src = 'bug.png';
     document.addEventListener('keydown', (e) => {
-
       switch (e.code) {
         case 'ArrowUp':
           if (this.playerY > 0 && this.jumpCount < 2) {
             this.playerY -= 100;
             this.jumpCount++;
-          } else if(this.playerY == innerHeight - 100){
+          } else if (this.playerY == innerHeight - 100) {
             this.jumpCount = 0;
           }
           break;
@@ -83,25 +80,25 @@ const mainView = {
             this.playerX += 5;
           }
           break;
-        case "KeyA": //left
+        case 'KeyA': // left
           if (this.playerX > 0) {
             this.playerX -= 5;
           }
           break;
-        case "KeyD"://right
+        case 'KeyD':// right
           if (this.playerX + 100 < innerWidth) {
             this.playerX += 5;
           }
           break;
-        case "Space"://jump
+        case 'Space':// jump
           if (this.playerY > 0 && this.jumpCount < 2) {
             this.playerY -= 100;
             this.jumpCount++;
-          } else if(this.playerY == innerHeight - 100){
+          } else if (this.playerY == innerHeight - 100) {
             this.jumpCount = 0;
           }
           break;
-        case "KeyS"://down
+        case 'KeyS':// down
           if (this.playerY + 100 < innerHeight) {
             this.playerY += 10;
           }
@@ -110,9 +107,17 @@ const mainView = {
     });
   },
   render: () => {
-    if (this.playerY + 100 < innerHeight) {
+    // Make the player fall slowly
+    if (this.playerY + innerHeight / 5 < innerHeight) {
       this.playerY += 2;
     }
+    if ((this.playerY + 50 > bugY && this.playerY - 50 < bugY) && (this.playerX + 50 > bugX && this.playerX - 50 < bugX)){
+      this.bug.src = '';
+    }
+    if (this.bugY + innerHeight / 5 < innerHeight) {
+      this.bugY += 2;
+    }
+    c.drawImage(this.floor, 0, innerHeight - (innerHeight / 5), innerWidth, innerHeight / 4);
     c.drawImage(this.bug, this.bugX, this.bugY, 100, 100);
     c.drawImage(this.player, this.playerX, this.playerY, 100, 100);
   }
