7aa60473cc6911b989cc6748ed7aa6b087ff4f26
Michael55555 <michaelpeters842@gmail.com>
2018-07-02 22:59:57 +0200

    New sprites and motion added

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 let model = {
+  collisionTime: 0,
   clickCount: 0,
-  level: innerHeight - (innerHeight / 5),
+  level: innerHeight - 100,
   playerSpeedX: 0,
   playerSpeedY: 0,
-  score: 0
+  score: 0,
+  sprites: {
+    bug: [
+      'bug0000.png',
+      'bug0006.png',
+      'bug0012.png',
+      'bug0018.png',
+      'bug0024.png',
+      'bug0030.png',
+      'bug0036.png',
+      'bug0042.png'
+    ],
+    hola: {},
+    run: {},
+    seated: {},
+    shoot: {},
+    test_mec: {},
+    walk: {},
+    main: {},
+  }
 }
 
 let controller = {
@@ -12,15 +32,14 @@ let controller = {
     this.c = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-    canvas.addEventListener('click', event => {
+    /*canvas.addEventListener('click', event => {
       this.x = event.pageX;
       this.y = event.pageY;
       if (this.x < canvas.width / 2 && this.y > canvas.height / 2.5) {} // left
       if (this.x > canvas.width / 2 && this.y > canvas.height / 2.5) {} // right
       if (this.y < canvas.height / 2.5) {} // jump
     });
-
-    if (window.innerHeight < 900) {} else {} // Device width / height specifications
+    if (window.innerHeight < 900) {} else {}*/ // Device width / height specifications
     mainView.init();
     setInterval(controller.animate, 20);
   },
@@ -31,22 +50,18 @@ let controller = {
     this.c.clearRect(0, 0, canvas.width, canvas.height);
     if (mainView.keys && mainView.keys[65]) { model.playerSpeedX = -2; } // left
     if (mainView.keys && mainView.keys[68]) { model.playerSpeedX = 2; } // right
-    if (mainView.keys && mainView.keys[87]) { model.playerSpeedY = -2; }  // up
+    if (mainView.keys && mainView.keys[87]) { if ((model.jumpCount < 20)) { model.playerSpeedY = -6; model.jumpCount++; } else { playerSpeedY = 0; } }  // up
     if (mainView.keys && mainView.keys[83]) { model.playerSpeedY = 2; } // down
     mainView.render();
   }
 }
-
 const mainView = {
   init: () => {
+    playerView.init();
     bugView.init();
     this.jumpCount = 0;
     this.floor = new Image();
-    this.floor.src = 'floor.png';
-    this.playerX = 0;
-    this.playerY = model.level - 100;
-    this.player = new Image();
-    this.player.src = 'developer.png';
+    this.floor.src = 'blocks/_rocky/rocky01.png';
     window.addEventListener('keydown', e => {
       mainView.keys = (mainView.keys || []);
       mainView.keys[e.keyCode] = (e.type == "keydown");
@@ -54,35 +69,71 @@ const mainView = {
     window.addEventListener('keyup', e => {
       if (mainView.keys && mainView.keys[65]) { model.playerSpeedX = 0; } // left
       if (mainView.keys && mainView.keys[68]) { model.playerSpeedX = 0; } // right
-      if (mainView.keys && mainView.keys[87]) { model.playerSpeedY = 0; }  // up
       if (mainView.keys && mainView.keys[83]) { model.playerSpeedY = 0; } // down
       mainView.keys[e.keyCode] = (e.type == "keydown");
     });
   },
   render: () => {
-    if (this.playerX + model.playerSpeedX <= innerWidth - 100 && this.playerX + model.playerSpeedX >= 0) {
-      this.playerX += model.playerSpeedX * 2;
-    }
-    if(this.playerY + model.playerSpeedY <= innerHeight - ((innerHeight / 5) + 100) && this.playerY + model.playerSpeedY >= 0){
-      this.playerY += model.playerSpeedY;
-    }
     if (this.bugY + 100 < model.level) {
       this.bugY += 2;
     }
+    playerView.render();
     bugView.render();
     if ((this.playerY + 50 > bugY && this.playerY - 50 < bugY) && (this.playerX + 50 > bugX && this.playerX - 50 < bugX)) {
-      this.bugX = Math.random() * innerWidth - 100;
-      this.bugY = Math.random() * innerHeight - ((innerHeight / 5) + 100);
+      this.bugX = Math.random() * (innerWidth - 100);
+      this.bugY = Math.random() * (innerHeight - 200);
       model.score++;
     }
     c.font = 'bold 42px Arial';
     c.fillStyle = 'white';
     c.fillText('Punkte: ' + model.score, 100, 100);
-    c.drawImage(this.floor, 0, model.level, innerWidth, innerHeight / 4);
-    c.drawImage(this.player, this.playerX, this.playerY, 100, 100);
+    for (let i = 0; i < 200; i++) {
+      c.drawImage(this.floor, i * 100, innerHeight - 100, 100, 100);
+    }
   }
 }
 
+const playerView = {
+  init: () => {
+    this.player = new Image();
+    this.player.src = 'Gbot_2d/pose_origin.png';
+    this.playerX = 0;
+    this.playerY = model.level - 127;
+    model.playerSpeedY = 0;
+  },
+  render: () => {
+    switch (true) {
+      case (model.playerSpeedY < 0):
+        this.player.src = 'Gbot_2d/jump_up.png';
+        break;
+        case (model.playerSpeedY > 0 && this.playerY + 127 != model.level):
+        this.player.src = 'Gbot_2d/jump_down.png';
+        break;
+        case (model.playerSpeedY > 0 && this.playerY + 127 == model.level):
+        this.player.src = 'Gbot_2d/pose_origin.png';
+        break;
+    }
+    if (model.jumpCount == 20) {
+      model.playerSpeedY = 0;
+    }
+    if (model.playerSpeedY <= 0) {
+      model.playerSpeedY += 2;
+    }
+    if (this.playerY + 127 == model.level) {
+      model.jumpCount = 0;
+    } else {
+      model.playerSpeedY - 1;
+    }
+    if (this.playerX + (model.playerSpeedX + 2) <= innerWidth - 100 && this.playerX + model.playerSpeedX >= 0) {
+      this.playerX += model.playerSpeedX;
+    }
+    // 850 + 2 = 852 && 900 - 127
+    if (this.playerY + model.playerSpeedY <= (innerHeight - (113 * 2)) && this.playerY + model.playerSpeedY >= 0) {
+      this.playerY += model.playerSpeedY;
+    }
+    c.drawImage(this.player, this.playerX, this.playerY, 150, 150);
+  }
+}
 const bugView = {
   init: () => {
     this.bug = new Image();
